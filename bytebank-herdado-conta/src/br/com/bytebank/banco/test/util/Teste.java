dc4e90afb8003ebe4cb8bd31aaa8ad7f1d1b0af3
Watson <watsontrovati@gmail.com>
2020-03-16 17:55:33 -0300

    Aprenbdendo AutoBoxing

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -0,0 +1,21 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		int [] idades = new int [5];
+		String [] nomes = new String [5];
+		
+		int idade = 29; //Integer
+		
+		Integer idadeRef = new Integer(29);
+		
+		
+		List <Integer> numeros = new ArrayList<Integer>();
+		numeros.add(29);
+		
+	}
+}

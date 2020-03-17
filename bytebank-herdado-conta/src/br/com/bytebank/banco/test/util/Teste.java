b0aedd58407daf14c2bc39837bc300339bfbdeb1
Watson <watsontrovati@gmail.com>
2020-03-16 21:00:07 -0300

    Comparator com numeros

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -1,23 +1,72 @@
 package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class Teste {
 	public static void main(String[] args) {
 		
-		int [] idades = new int [5];
-		String [] nomes = new String [5];
+		Conta cc1 = new ContaCorrente(22, 33);
+		cc1.deposita(333.0);
 		
-		int idade = 29; //Integer
+		Conta cc2 = new ContaPoupanca(22, 44);
+		cc2.deposita(444.0);
 		
-		Integer idadeRef = Integer.valueOf(29); //autoboxing
-		int valor = idadeRef.intValue(); //unboxing
+		Conta cc3 = new ContaCorrente(22, 11);
+		cc3.deposita(111.0);
 		
-		String s = args[0];
+		Conta cc4 = new ContaPoupanca(22, 22);
+		cc4.deposita(222.0);
 		
-		List <Integer> numeros = new ArrayList<Integer>();
-		numeros.add(29);
+		List<Conta> lista = new ArrayList<>();
 		
+		lista.add(cc1);
+		lista.add(cc2);
+		lista.add(cc3);
+		lista.add(cc4);
+		
+		for (Conta conta : lista) {
+			
+			System.out.println(conta);
+			
+		}
+		
+		NumeroComparator comparator = new NumeroComparator();
+		
+	    Collections.sort(lista, comparator);
+		
+		System.out.println("----------------------");
+		
+		for (Conta conta : lista) {
+			
+			System.out.println(conta);
+			
+		}
+								
 	}
+}
+
+class NumeroComparator implements Comparator <Conta>{
+
+	@Override
+	public int compare(Conta c1, Conta c2) {
+		
+		if(c1.getNumero() < c2.getNumero()) {
+			
+			return -1;
+		}
+		if (c1.getNumero() > c2.getNumero()) {
+			return 1;
+		}
+		
+		return 0;
+	}
+	
+	
 }
